f344599803b2711847f9b857b03bce8b0ada154c
meidicup <g.h.wonderfulworld+hobby@gmail.com>
2025-05-21 20:49:20 +0900

    Update script.js

--- meidicup/script.js
+++ meidicup/script.js
@@ -59,9 +59,8 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   };
 });
-
-  function submitted() {
-    // 送信後に何か表示したいならここに処理を書く
-    console.log("フォームが送信されました");
-  }
+function submitted() {
+  // 送信後に何か表示したいならここに処理を書く
+  console.log("フォームが送信されました");
+}
 
